3a2268650601058bab3994f1af81a7d1dcbae98a
ne0rad <usackasr@gmail.com>
2022-01-07 21:15:28 +0000

    Adds <hr /> after every message except last

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,8 @@
-function Message({ message }) {
+function Message({ message, isLast }) {
     return (
         <div className="message">
             <span className="message-text">[admin]: {message.message}</span>
+            {!isLast && <hr />}
         </div>
     )
 }
