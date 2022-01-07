6ecab5927c44a7c675327eed3eb6a1ceeaba8b42
ne0rad <usackasr@gmail.com>
2022-01-07 21:15:09 +0000

    Updated Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,50 @@
-function Home() {
+import { useState } from "react";
+
+function Home({ updateUser }) {
+
+    const [username, setUsername] = useState("");
+    const [roomID, setRoomID] = useState("");
+
     return (
         <div className="home">
-            <h1 className="text-title">Quick Chat</h1>
+            <h1 className="text-title">Live Chat</h1>
             <br />
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form
+                className="container"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    updateUser({username: username, roomID: roomID});
+                }}
+            >
                 <span className="text-label">Username: </span>
                 <br />
-                <input className="input" required />
+                <input
+                    type="text"
+                    className="input"
+                    spellCheck="false"
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.currentTarget.value)}
+                />
                 <br />
                 <br />
                 <span className="text-label">Room ID*: </span>
                 <br />
-                <input className="input" required />
+                <input
+                    type="text"
+                    className="input"
+                    spellCheck="false"
+                    required
+                    value={roomID}
+                    onChange={(e) => setRoomID(e.currentTarget.value)}
+                />
                 <br />
                 <br />
                 <button className="btn btn-connect" type="submit">Connect</button>
             </form>
             <br />
             <p className="text-small">* room ID can be any word or phrase.</p>
-            <p className="text-small">* Others will be able connect based on Room ID.</p>
+            <p className="text-small">* others will be able connect based on room ID.</p>
         </div>
     )
 }
