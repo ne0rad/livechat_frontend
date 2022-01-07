6372fc0a86df18fd42194bc50716176c7bdf96f3
ne0rad <usackasr@gmail.com>
2022-01-07 21:14:33 +0000

    Using LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { SocketContext, socket } from "./socket/socket";
 import Home from "./components/Home";
+import Chat from "./components/Chat";
 
 function App() {
-  const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState();
 
+  // useEffect(() => {
+  //   socket.on("chat", newMessage => {
+  //     const messagesTemp = [...messages];
+  //     if (messagesTemp.length > 100) messagesTemp.splice(0, 1);
+  //     messagesTemp.push(newMessage);
+  //     setMessages(messagesTemp);
+  //   });
+  //   return () => {
+  //     socket.off("chat");
+  //   }
+  // }, [messages]);
+
+  // Check if user has any data in local storage.
+  // Update app state if they do.
   useEffect(() => {
-    socket.on("chat", newMessage => {
-      const messagesTemp = [...messages];
-      if (messagesTemp.length > 100) messagesTemp.splice(0, 1);
-      messagesTemp.push(newMessage);
-      setMessages(messagesTemp);
-    });
-    return () => {
-      socket.off("chat");
-    }
-  }, [messages]);
+    let username = window.localStorage.getItem('username');
+    let roomID = window.localStorage.getItem('roomID');
 
+    if (username && roomID) {
+      setUser({ username: username, roomID: roomID });
+    }
+
+    return () => {
+      setUser();
+    }
+  }, []);
+
+  function updateUser(userData) {
+    if(!userData || !userData.username || !userData.roomID) return;
+    setUser(userData);
+  }
 
   return (
     <SocketContext.Provider value={socket}>
       <div className="main">
-        <Home />
+        {
+          user ?
+            (<Chat user={user}/>)
+            :
+            (<Home updateUser={updateUser}/>)
+        }
       </div>
     </SocketContext.Provider>
   );
