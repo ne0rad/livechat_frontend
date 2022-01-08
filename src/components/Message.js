535866021bf65b42e37b33ab5a6fee3c78ce0e10
ne0rad <usackasr@gmail.com>
2022-01-08 20:56:33 +0000

    Updated Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,7 @@
 function Message({ message, isLast }) {
     return (
         <div className="message">
-            <span className="message-text">[admin]: {message.message}</span>
+            <span className="text-message"><b>{message.username}</b>: {message.message}</span>
             {!isLast && <hr />}
         </div>
     )
